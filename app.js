51eccaa96fdfbf758b82ffa67fcfe8bbdb35e686
MaggieWesolowska <maggiewes07@gmail.com>
2023-08-16 20:36:55 +0200

    edited routes

--- app.js
+++ app.js
@@ -18,15 +18,36 @@ app.use(express.json());
 require('./config/config-passport');
 
 app.use('/api/contacts', contactsRouter);
-app.use('/api/users/signup', usersRouter);
-app.use('/api/users/login', usersRouter);
+app.use('/api/users', usersRouter);
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: 'error',
+    code: 404,
+    message: `Use api on routes: 
+    /users/signup - registration user {username, email, password}
+    /users/login - login {email, password}
+    /users/current - get message if user is authenticated`,
+    data: 'Not found',
+  });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.use((err, _, res, __) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: 'fail',
+    code: 500,
+    message: err.message,
+    data: 'Internal Server Error',
+  });
 });
+
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Not found' });
+// });
+
+// app.use((err, req, res, next) => {
+//   res.status(500).json({ message: err.message });
+// });
 
 module.exports = app;
