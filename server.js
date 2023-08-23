925a5c2559900c2990ad0b40229d9c4ac26e667d
MaggieWesolowska <maggiewes07@gmail.com>
2023-08-23 14:50:50 +0200

    initialized branch, added dependencies

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require('./app');
+const path = require('path');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const createFolderIfNotExist = require('./config/config-multer');
 mongoose.Promise = global.Promise;
+
+const uploadDir = path.join(process.cwd(), 'uploads');
+const storeImage = path.join(process.cwd(), 'images');
 
 const PORT = process.env.PORT || 3000;
 const uriDb = process.env.DB_HOST;
@@ -16,6 +21,8 @@ connection
   .then(() => {
     console.log('Database connection successful');
     app.listen(PORT, () => {
+      createFolderIfNotExist(uploadDir);
+      createFolderIfNotExist(storeImage);
       console.log(`App listens on port: ${PORT}`);
     });
   })
