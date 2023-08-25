45d38024df96a1e8a3371c7354bbadb7521946e7
MaggieWesolowska <maggiewes07@gmail.com>
2023-08-25 18:07:02 +0200

    edited middleware

--- middleware/validateAuth.js
+++ middleware/validateAuth.js
@@ -32,32 +32,3 @@ const auth = (req, res, next) => {
 };
 
 module.exports = auth;
-
-const passport = require('passport');
-require('dotenv').config();
-
-const auth = (req, res, next) => {
-  passport.authenticate(
-    'jwt',
-    { session: false },
-    (err, user) => {
-      const token = req
-        .get('Authorization')
-        .replace('Bearer ', '');
-      console.log(token);
-      console.log(user);
-      if (!user || err || !token || token !== user.token) {
-        return res.status(401).json({
-          status: 'error',
-          code: 401,
-          message: 'Unauthorized',
-          data: 'Unauthorized',
-        });
-      }
-      req.user = user;
-      next();
-    }
-  )(req, res, next);
-};
-
-module.exports = auth;
