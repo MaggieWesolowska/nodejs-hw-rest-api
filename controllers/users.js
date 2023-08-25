99261931d13c76576f3aa6918623aaa8ef61d81b
MaggieWesolowska <maggiewes07@gmail.com>
2023-08-25 18:03:59 +0200

    Merge branch 'master' into hw05-avatars

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,6 @@ const User = require('../models/user');
 const gravatar = require('gravatar');
 
 require('dotenv').config();
-
 const secret = process.env.SECRET;
 
 const listUsers = async (req, res) => {
